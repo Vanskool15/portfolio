4b864b89dd6b5dd4abcc315eede391b8bb51dc78
Vanskool15 <darrellnew2018@gmail.com>
2024-04-20 16:42:17 +0800

    new design

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var loader = document.getElementById("preloader");
-
-window.addEventListener("load", ()=> {
-    loader.style.display = "none";
-})
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
